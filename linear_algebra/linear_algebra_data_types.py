31b58bf3350168edb3525e7a42387f72bdc06530
Michael-MD <mdei0001@student.monash.edu>
2022-12-20 16:54:11 +1100

    rearranged files to fix reflection bug.

--- linear_algebra/linear_algebra_data_types.py
+++ linear_algebra/linear_algebra_data_types.py
@@ -5,7 +5,7 @@ mag = np.linalg.norm
 dot = np.dot
 cross = np.cross
 inv = np.linalg.inv
-EPSILON = 1e-5
+EPSILON = .1
 
 def vector(x,y,z): return np.array([x,y,z,0])
 
@@ -13,8 +13,12 @@ def point(x,y,z): return np.array([x,y,z,1])
 
 def color(x,y,z): return np.array([x,y,z])
 
+black = color(0,0,0)
+white = color(1,1,1)
+
 def normalize(v):
 	if mag(v) < EPSILON:
-		raise ZeroDivisionError("Cannot normalizing zero vector.")
+		return v
+		# raise ZeroDivisionError("Cannot normalizing zero vector.")
 	else:
 		return v/mag(v)
--- render/render_scene.py
+++ render/render_scene.py
@@ -4,13 +4,13 @@ path.append("../ray_casting")
 path.append("../linear_algebra")
 path.append("../lights")
 
-from surface_lighting import *
 from linear_algebra import *
 from ppm_file import *
 from cast_ray_to_canvas import *
 from ray_intersection import *
 from world import *
 from group_transform import *
+from reflect import reflect
 
 
 def render(cam, w):
@@ -35,14 +35,6 @@ def is_shadowed(w,p):
 		return True
 	else:
 		return False
-
-
-def color_at(w,r,remaining = 5):
-	xs = intersect_world(w,r)
-	x = hit(xs)
-	if x == None: return color(0,0,0)
-	comps = prepare_computations(x,r,xs)
-	return shade_hit(w,comps,remaining)
 
 
 class prepare_computations:
@@ -104,3 +96,60 @@ def shade_hit(w,comps, remaining):
 	else:
 		c =  surface + reflected + refracted
 	return np.minimum( c, color(1,1,1) )
+
+
+
+def color_at(w,r,remaining = 5):
+	xs = intersect_world(w,r)
+	x = hit(xs)
+	if x == None: return color(0,0,0)
+	comps = prepare_computations(x,r,xs)
+	return shade_hit(w,comps,remaining)
+
+def lighting(object, light,p,eyev,normalv,in_shadow = False):
+	mat = object.mat
+	effective_color = mat.pat.pattern_at_object(object,p) * light.intensity
+	lightv = normalize( light.position - p )
+	ambient = effective_color * mat.ambient
+	light_dot_normal = dot(lightv,normalv)
+	if light_dot_normal < 0 or in_shadow:
+		specular = diffuse = color(0,0,0)
+	else:
+		diffuse = effective_color * mat.diffuse * light_dot_normal
+		reflectv = reflect(-lightv,normalv)
+		reflect_dot_eye = dot(reflectv,eyev)
+
+		if reflect_dot_eye <= 0:
+			specular = color(0,0,0)
+		else:
+			factor =  reflect_dot_eye ** mat.shininess 
+			specular = light.intensity * mat.specular * factor
+
+	return ambient + diffuse + specular
+
+
+
+def reflected_color(w,comps,remaining):
+	if comps.object.mat.reflective < EPSILON: return color(0,0,0)
+	reflect_ray = ray(comps.over_point,comps.reflectv)
+	if remaining<=0: c = color(0,0,0)
+	else: c = color_at( w,reflect_ray,remaining-1 )
+	return c * comps.object.mat.reflective
+
+
+def refracted_color(w,comps,remaining):
+	if comps.object.mat.transparency == 0 or remaining <= 0:
+		return color(0,0,0)
+
+	n_ratio = comps.n1 / comps.n2
+	cos_i = dot(comps.eyev, comps.normalv)
+	sin2_t = n_ratio**2 * (1 - cos_i**2)
+
+	if sin2_t > 1:
+		return color(0,0,0)
+
+	cos_t = np.sqrt(1.0 - sin2_t)
+	direction = comps.normalv * (n_ratio * cos_i - cos_t) - comps.eyev * n_ratio
+	refract_ray = ray(comps.under_point, direction)
+	c = color_at(w, refract_ray, remaining - 1)
+	return c * comps.object.mat.transparency
